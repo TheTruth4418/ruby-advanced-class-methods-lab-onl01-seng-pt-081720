1c553ccc1b1859a415d7528409e8ce2d2176a0c5
William West <wwest466@gmail.com>
2020-09-04 13:40:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,27 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    name = self.new
+    @@all << name
+    return name
+  end
+
   def self.all
     @@all
   end
+
+def self.new_by_name(title)
+  song = self.new
+  song.name = title
+  song
+end
+
+def self.create_by_name(title)
+  song = self.new
+  song.name = title
+  @@all << song
+end
 
   def save
     self.class.all << self
